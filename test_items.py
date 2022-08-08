606fe6e52447c174f904fce9430421876703dc89
a.vohmin <ecZF3s498q!>
2022-08-08 18:03:55 +0700

    Переписание кода под условия проверки задачи

--- test_items.py
+++ test_items.py
@@ -2,9 +2,14 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_lang(browser):
+def test_add_item_to_basket_button(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.implicitly_wait(10)
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, 'button.btn.btn-lg.btn-primary.btn-add-to-basket')
-    time.sleep(2) # пауза чтобы увидеть язык сайта
+
+    # чтобы по заданию корректно отработал assert пришлось получать массив кнопок по селектору и сравнивать с 0
+    assert len(browser.find_elements(By.CSS_SELECTOR, 'button.btn.btn-lg.btn-primary.btn-add-to-basket')) != 0, "Кнопка добавления товара в корзину отсутствует"
+    # browser.find_element(By.CSS_SELECTOR, 'button.btn.btn-lg.btn-primary.btn-add-to-basket') # <- вполне рабочий вариант
+
+    time.sleep(5) # пауза чтобы увидеть язык сайта
+    # не стал ставить 30 секунд как по заданию, ибо можно уснуть в ожидании, а иначе процессы вебдрайвера не завершатся
